02a2ba049f09c4ea210773e330131c1d4a9a134c
Chris Preston <jogobom@gmail.com>
2023-04-07 07:52:19 +0100

    First commit

---
+++ Tests/KeyReformatterTests.cs
@@ -0,0 +1,17 @@
+using FluentAssertions;
+
+namespace Tests;
+
+public class KeyReformatterTests
+{
+    [Theory]
+    [InlineData("key", "key")]
+    [InlineData("a key", "aKey")]
+    [InlineData("a more complex key", "aMoreComplexKey")]
+    [InlineData("aCorrectKey", "aCorrectKey")]
+    [InlineData("a key: with a colon", "aKeyWithAColon")]
+    public void ToSnakeCase(string input, string expectedResult)
+    {
+        input.ToSnakeCase().Should().Be(expectedResult);
+    }
+}
---
+++ Updater/KeyReformatter.cs
@@ -0,0 +1,20 @@
+public static class KeyReformatter
+{
+    public static string ToSnakeCase(this string key)
+    {
+        var delimeters = new[] { ' ', ':' };
+        var words = key.Split(delimeters, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        var firstWord = words.Take(1);
+        var subsequentWords = words.Skip(1).Select(CapitalizeFirstLetter);
+        
+        return string.Join(string.Empty, firstWord.Concat(subsequentWords));
+    }
+
+    private static string CapitalizeFirstLetter(string word)
+    {
+        var firstLetter = word.Take(1).Select(char.ToUpper);
+        var subsequentLetters = word.Skip(1);
+        return new string(firstLetter.Concat(subsequentLetters).ToArray())?? word;
+    }
+}
